9b2749c52a19c8602b06378f2bb4acd63c525032
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2025-01-11 10:21:04 +0330

    step two, main page

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
 const App = () => {
+
+const {width, height} = CustomHook()
+
   return (
-    <div className='text-red-900'>App</div>
+    <div className='flex flex-col items-center justify-center h-screen w-screen gap-4'>
+    <h1 className='font-bold text-3xl'>"Use Window resize Hook"</h1>
+    <p className='text-2xl'>Width is : <span className='text-blue-800 font-bold'>{width}</span></p>
+    <p className='text-2xl'>height is : <span className='text-blue-800 font-bold'>{height}</span></p>
+    </div>
   )
 }
 
